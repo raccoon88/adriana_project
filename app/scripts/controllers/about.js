c6e744dce5c149210accf59108306dfe4d29cd8d
britishd <vpakin@gmail.com>
2015-04-05 14:41:36 +0300

    Create a new clean angular app

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name adrianaProjectApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the adrianaProjectApp
+ */
+angular.module('adrianaProjectApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name adrianaProjectApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the adrianaProjectApp
+ */
+angular.module('adrianaProjectApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = ['macbook','friend','woman', 'travel'];
+  });
